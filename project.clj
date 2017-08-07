7c58b0aaa393f7eb28843c8bb5d591ee15a6bce3
mars0i <mabrams@uab.edu>
2017-08-07 12:18:11 -0500

    more work on pinv

--- project.clj
+++ project.clj
@@ -4,10 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 ;[net.mikera/cljutils "0.8.0-SNAPSHOT"]
                  [net.mikera/core.matrix "0.60.3"]
                  ;[net.mikera/core.matrix "0.60.4-SNAPSHOT"]
                  [net.mikera/vectorz-clj "0.47.0"]
                  [clatrix "0.5.0"]
+                 [thinktopic/aljabr "0.1.2-SNAPSHOT"]
                  ;[org.nd4j/nd4j-native "0.5.0"]
                  ;[org.clojars.ds923y/nd4clj "0.1.1-SNAPSHOT"] 
                 ])
--- src/matexp/moorepenrose.clj
+++ src/matexp/moorepenrose.clj
@@ -1,6 +1,7 @@
 (ns matexp.moorepenrose
-  (:require [clojure.core.matrix :as mx]
-            [clojure.core.matrix.linear :as lin]))
+  (:require [clojure.core.matrix :as m]
+            [clojure.core.matrix.linear :as lin]
+            [clojure.core.matrix.utils :as u]))
 
 
 ;; Questions:
@@ -8,22 +9,25 @@
 ;; Complex matrices are not fully part of core.matrix at present, right?
 ;; So I should use transpose rather than hermitian-transpose?
 ;; Eventually this needs to use hermitian-transpose on complex matrices.
+;; Could use doseq-indexed from Mikera's cljutils for the loop.
 (defn pinv 
-  "Moore-Penrose pseudoinverse of matrix m, calculated using svd.  tolerance 
-  defaults to [FIXME]; during calculation, values below tolerance in the sigma
-  matrix produced by svd are treated as zero."
+  "Moore-Penrose pseudoinverse of matrix m calculated using svd.  tolerance 
+  defaults to 0 during calculation. Absolute values of singular values that 
+  are less than or equal to tolerance will be treated as zero.  Note requires
+  an implementation that includes svd."
   ([m] (pinv m 0.0)) ;; default tolerance should be larger--maybe calculated
   ([m tolerance]
-   (let [treat-as-nonzero?  (fn [x] (> (Math/abs x) tolerance))
-         diag-pinv (fn [singular-vals sigma-rows sigma-cols] 
+   (let [treat-as-nonzero? (fn [x] (> (Math/abs x) tolerance))
+         diag-pinv (fn [singular-vals rows cols] 
                      (let [smaller-dim (min rows cols)
-                           sigma+ (ensure-mutable (zero-matrix cols rows))]
-                       (doseq [i (range smaller-dim)
-                               :let [x (mx/mget singular-vals i)]
-                               :while (treat-as-nonzero? x)]
-                         (mx/mset! sigma+ i i (/ x)))))
+                           sigma+ (m/ensure-mutable (m/zero-matrix rows cols))]
+                       (u/doseq-indexed [x singular-vals i]
+                          (when (treat-as-nonzero? x)
+                            (m/mset! sigma+ i i (/ x))))
+                       sigma+))
+         [rows cols] (m/shape m)
          {:keys [U S V*]} (lin/svd m)
-         rows (second (shape U)) ; FIXME
-         cols (first (shape V*)) ; FIXME
+         _ (println (m/shape U) (m/shape m) (m/shape V*)) ; DEBUG
          S+ (diag-pinv S rows cols)]
-     (mx/mmul (mx/transpose V*) S+ (mx/transpose U)))))
+     (println S+)
+     (m/mmul (m/transpose V*) S+ (m/transpose U)))))
